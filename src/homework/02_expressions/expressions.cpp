d06c3717af68928b747fc91ba1c13cb2d6bbeab6
Wayne Stovey <veysto2@gmail.com>
2022-02-17 16:49:34 -0600

    HW02-expressions

--- src/homework/02_expressions/expressions.cpp
+++ src/homework/02_expressions/expressions.cpp
@@ -9,6 +9,20 @@ int add_numbers(int num1, int num2)
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
 
+/*multiply tax_rate and meal_amount and return their
+product.*/
+double get_sales_tax_amount(double meal_amount)
+{
+	double tax_rate = .0675;
+	return tax_rate * meal_amount;
+}
+
+/*create a double value-return function prototype named 
+get_tip_amount with two double parameters: meal_amount and tip_rate.*/
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
 
 
 
--- src/homework/02_expressions/expressions.h
+++ src/homework/02_expressions/expressions.h
@@ -3,6 +3,10 @@ int add_numbers(int num1, int num2);
 
 //write function prototype for multiply_numbers with two integer parameters num1, and num2
 
+/*create a double value-return function prototype named 
+get_sales_tax_amount with one double parameter: meal_amount.*/
+double get_sales_tax_amount(double meal_amount);
 
-
-
+/*create a double value-return function prototype named 
+get_tip_amount with two double parameters: meal_amount and tip_rate.*/
+double get_tip_amount(double meal_amount, double tip_rate);
