85d84bf7a868696f5b88aa4a1651cfc47aba3ca6
Wayne Stovey <veysto2@gmail.com>
2022-02-17 17:08:15 -0600

    HW02-expressions

--- test/homework_test/02_expressions_test/02_expressions_tests.cpp
+++ test/homework_test/02_expressions_test/02_expressions_tests.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "variables.h"
-#include "expressions.h"
+#include "../src/homework/02_expressions/expressions.h" 
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
