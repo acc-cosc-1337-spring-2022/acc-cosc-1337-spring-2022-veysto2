ae3c8ead4b572b0cd7c2ba2fe2c49f68e6abafec
Wayne Stovey <veysto2@gmail.com>
2022-02-17 12:29:18 -0600

    HW-01

--- src/homework/01_variables/variables.h
+++ src/homework/01_variables/variables.h
@@ -1,6 +1,10 @@
 ﻿//example
 int add_numbers(int num1, int num2);
 
+// multiply
+int multiply_numbers(int num1);
+
+
 
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,14 +1,16 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "expressions.h"
 
 //write namespace using statement for cout
-
+using std::cout;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
+	int multiply_numbers(int num1, int num2); 	
 	
 
 	return 0;
