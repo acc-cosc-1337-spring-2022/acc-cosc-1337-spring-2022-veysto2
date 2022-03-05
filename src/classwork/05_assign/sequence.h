6b8777a2542dc03ace89846732df5a01d5b958db
Wayne Stovey <veysto2@gmail.com>
2022-03-05 02:33:42 -0600

    Hmw05

--- src/classwork/05_assign/sequence.h
+++ src/classwork/05_assign/sequence.h
@@ -11,4 +11,28 @@ Write prototype for void function update_vector_element that accepts parameters 
 a string search_value, and a string replace_value.
 */
 
+/*Create a function with a string parameter, process string for reverse complement, and return a string.
+create a function with a const reference string parameter, write code to calculate the GC-content, and return it. */
+
+#include <iostream>
+#include <string>
+using std::cout; using std::cin; using std::string;
+
+//prototypes
+
+double get_gc_content();
+
+string get_reverse_string();
+
+string get_dna_complement();
+
+void promt_user();
+
+void display_menu();
+
+void handle_menu_option(int option);
+
+void run_menu();
+
+
 
