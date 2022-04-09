9915e4827116c3257cad7db68abcac80fcd136a2
Wayne Stovey <veysto2@gmail.com>
2022-04-09 04:17:42 -0500

    Hmw08

--- src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,46 @@
 //cpp
+#include "tic_tac_toe_manager.h"
+#include <iostream>
+using std::cout, std::string, std::cin, std::vector, std::ostream, std::istream;
+
+std::ostream& operator<<(std::ostream& out, const TicTacToeManager& manager)
+{
+    for (auto i = manager.games.begin(); i != manager.games.end(); ++i )
+    {
+        out << *i << "\n"; 
+    }
+
+    return out; 
+}
+
+
+void TicTacToeManager::save_game(TicTacToe b) 
+{
+    games.push_back(b);
+    update_winner_count(b.get_winner());
+}
+
+void TicTacToeManager::get_winner_total(int& o, int& x, int& t)
+{
+    x = x_win;
+    o = o_win;
+    t = ties;
+    cout << "O wins: " << o << "\n";
+    cout << "X wins: " << x << "\n";
+    cout << "Ties: " << t << "\n";
+}
+
+
+void TicTacToeManager::update_winner_count(std::string winner)
+{
+    if (winner == "X") 
+    {
+        x_win+=1;
+    } else if (winner == "O") 
+    {
+        o_win+=1;
+    } else if (winner == "C")
+    {
+        ties+=1;
+    }
+}
